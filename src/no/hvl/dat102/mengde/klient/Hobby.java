e269c1e55018503f52d12617e324a377d64476d6
h110541 <110541@stud.hvl.no>
2021-03-08 00:19:08 +0100

    Hobby

---
+++ src/no/hvl/dat102/mengde/klient/Hobby.java
@@ -0,0 +1,40 @@
+package no.hvl.dat102.mengde.klient;
+
+public class Hobby {
+
+    private String hobbyNavn;
+
+    public Hobby(String hobbyNavn) {
+        this.hobbyNavn = hobbyNavn;
+    }
+
+    @Override
+    public String toString() {
+        return ("<" + hobbyNavn + ">");
+    }
+
+    @Override
+    public boolean equals(Object hobby2) {
+
+        if (this == hobby2)
+            return true;
+
+        if (hobby2 == null)
+            return false;
+
+        if (getClass() != hobby2.getClass())
+            return false;
+
+        String hobby2Navn = ((Hobby) hobby2).getHobbyNavn();
+        return (hobbyNavn.equals(hobby2Navn));
+    }
+
+    public String getHobbyNavn() {
+        return hobbyNavn;
+    }
+
+    public void setHobbyNavn(String hobbyNavn) {
+        this.hobbyNavn = hobbyNavn;
+    }
+
+}
